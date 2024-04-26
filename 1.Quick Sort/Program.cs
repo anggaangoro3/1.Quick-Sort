e8d4982c630567f6f250e3d63efe0915b4b1fb95
Anggoro <Anggoro@LAPTOP-PU0HK8FK>
2024-04-27 02:27:06 +0700

    Add project files.

---
+++ 1.Quick Sort/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = new int[10];
+        Random rand = new Random();
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = rand.Next(1, 100);
+        }
+
+        Console.Write("Sebelum sort: ");
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + ", ");
+        }
+        Console.WriteLine();
+        QuickSort(array, 0, array.Length - 1);
+
+        Console.Write("Setelah sort: ");
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + ", ");
+        }
+        Console.WriteLine("\n");
+    }
+
+    static void QuickSort(int[] arr, int left, int right)
+    {
+        int l, r, s;
+        while (right > left)
+        {
+            l = left;
+            r = right;
+            s = arr[left];
+            while (l < r)
+            {
+                while (arr[r] > s)
+                {
+                    r--;
+                }
+                arr[l] = arr[r];
+                while (s >= arr[l] && l < r)
+                {
+                    l++;
+                }
+                arr[r] = arr[l];
+            }
+            arr[l] = s;
+            QuickSort(arr, left, l - 1);
+            left = l + 1;
+        }
+    }
+}
